b00dafda1517883fae0615bedba1210cdd6bc497
Roman Ledjajev <roman.ledjajev@tptlive.ee>
2025-12-05 10:11:21 +0200

    used links instead of partial inclusion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     {{-- Custom pagination --}}
-    @include('partials.pagination', ['paginator' => $posts])
+    {{ $posts->links() }}
 
     {{-- Posts grid --}}
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-2">
@@ -13,6 +13,6 @@
     </div>
 
     {{-- Custom pagination at bottom --}}
-    @include('partials.pagination', ['paginator' => $posts])
+    {{ $posts->links() }}
 
 @endsection
