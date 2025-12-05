3b4310928d88867f19e635b87ba8454f381ee0f5
Roman Ledjajev <roman.ledjajev@tptlive.ee>
2025-12-05 10:05:29 +0200

    implemented pagination partial into welcome blade view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,18 @@
 @extends('partials.layout')
 @section('title', 'Home')
 @section('content')
-        {{ $posts->links() }}
-        <div class="grid grid-cols-4 gap-2">
-            @foreach ($posts as $post)
-                 @include('partials.post-card')
-            @endforeach
-        </div>
+
+    {{-- Custom pagination --}}
+    @include('partials.pagination', ['paginator' => $posts])
+
+    {{-- Posts grid --}}
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-2">
+        @foreach ($posts as $post)
+            @include('partials.post-card')
+        @endforeach
+    </div>
+
+    {{-- Custom pagination at bottom --}}
+    @include('partials.pagination', ['paginator' => $posts])
 
 @endsection
